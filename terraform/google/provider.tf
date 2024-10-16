e6904679e29ee9e958f9ee3a0bef5885b74dbddc
Mystos16 <gijsvanzoelen@outlook.com>
2024-10-16 11:35:41 +0200

    d

---
+++ terraform/google/provider.tf
@@ -0,0 +1,6 @@
+# provider "google" {
+#   project     = "saxionclouds"
+#   region      = "europe-north1"
+#   zone = "europe-north1-a"
+#   credentials = file("C:/Users/Lucas/google/saxionclouds-ab524f132ccb.json")
+# }
