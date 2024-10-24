f321085b5c3aedf9f116ceb1b805db5ea998c330
Mystos16 <gijsvanzoelen@outlook.com>
2024-10-24 15:54:41 +0200

    l

--- awsBasics/backup.sh
+++ awsBasics/backup.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+# Variables
+STATIC_WEBSITE_BUCKET="<replace-with-static-website-bucket-name>"
+BACKUP_BUCKET="<replace-with-backup-bucket-name>"
+TIMESTAMP=$(date +"%Y-%m-%d-%H-%M-%S")
+BACKUP_PATH="s3://$BACKUP_BUCKET/backup-$TIMESTAMP/"
+
+# Backup command
+echo "Starting backup of $STATIC_WEBSITE_BUCKET to $BACKUP_PATH..."
+
+# Sync the content of the static website bucket to the backup bucket
+aws s3 sync s3://$STATIC_WEBSITE_BUCKET $BACKUP_PATH
+
+if [ $? -eq 0 ]; then
+    echo "Backup successful!"
+else
+    echo "Backup failed!"
+    exit 1
+fi
