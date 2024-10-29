1ab07c45984fa73acf3cb4211c2f503755c31026
Mystos16 <gijsvanzoelen@outlook.com>
2024-10-29 11:02:03 +0100

    Delete deploy.sh

--- dockerInTheCloud/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-aws cloudformation create-stack --stack-name CloudShirt --template-body dockerInTheCloud/dockerInTheCloudNetwerk.yaml
-aws cloudformation create-stack --stack-name CloudShirtDB --template-body dockerInTheCloud/dockerInTheCloudRDS.yaml
-aws cloudformation create-stack --stack-name CloudShirtJumpServer --template-body dockerInTheCloud/dockerInTheCloudJumpServer.yaml
-aws cloudformation create-stack --stack-name CloudShirtBuildServer --template-body dockerInTheCloud/dockerInTheCloudBuildServer.yaml
-aws cloudformation create-stack --stack-name CloudShirtASG --template-body dockerInTheCloud/dockerInTheCloudAutoScalingGroup.yaml
