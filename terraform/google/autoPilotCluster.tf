bb3db57cfc4e89dee2bf6af154150320b8e2c1bb
PinkBowser <lletterwinkel2@gmail.com>
2024-10-30 12:20:52 +0100

    probeersels lucas terraform

--- terraform/google/autoPilotCluster.tf
+++ terraform/google/autoPilotCluster.tf
@@ -1,5 +1,26 @@
-resource "google_container_cluster" "autopilot" {
-  name = "autopilot-cluster-cloudshirt"
-  enable_autopilot = true
-  deletion_protection = false
+#  resource "google_container_cluster" "autopilot" {
+#    name = "autopilot-cluster-cloudshirt"
+#    enable_autopilot = true
+#    deletion_protection = false
+#  }
+
+# resource "google_sql_database_instance" "autopilot-cloudshirt-database" {
+#   name             = "autopilot-cloudshirt-database"
+#   database_version = "MYSQL_8_0"
+#   root_password = "CloudShirt123"
+#   deletion_protection = false
+
+#   settings {
+#     tier = "db-f1-micro"
+#   }
+# }
+
+resource "google_artifact_registry_repository" "cloudshirt-repo" {
+  location      = "us-central1"
+  repository_id = "cloudshirt-repo"
+  description   = "cloudshirt-repo voor de cloudshirt website"
+  format        = "DOCKER"
 }
+
+# resource "kubernetes_deployment_v1" "default" {
+# }
