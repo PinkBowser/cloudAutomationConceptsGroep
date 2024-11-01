1fedacc7cc05533ef581242ddc26a076df7418ca
PinkBowser <lletterwinkel2@gmail.com>
2024-11-01 17:52:58 +0100

    ik ben boos op ansible

--- terraform/google/autoPilotCluster.tf
+++ terraform/google/autoPilotCluster.tf
@@ -1,26 +1,24 @@
-#  resource "google_container_cluster" "autopilot" {
-#    name = "autopilot-cluster-cloudshirt"
-#    enable_autopilot = true
-#    deletion_protection = false
-#  }
+ resource "google_container_cluster" "autopilot" {
+   name = "autopilot-cluster-cloudshirt"
+   enable_autopilot = true
+   deletion_protection = false
+ }
 
-# resource "google_sql_database_instance" "autopilot-cloudshirt-database" {
-#   name             = "autopilot-cloudshirt-database"
-#   database_version = "MYSQL_8_0"
-#   root_password = "CloudShirt123"
-#   deletion_protection = false
+resource "google_sql_database_instance" "autopilot-cloudshirt-database" {
+  name             = "autopilot-cloudshirt-database"
+  database_version = "SQLSERVER_2019_EXPRESS"
+  root_password = "CloudShirt123"
+  deletion_protection = false
+  region = "europe-west1"
 
-#   settings {
-#     tier = "db-f1-micro"
-#   }
-# }
+  settings {
+    tier = "db-custom-2-7680"
+  }
+}
 
 resource "google_artifact_registry_repository" "cloudshirt-repo" {
-  location      = "us-central1"
+  location      = "europe-west1"
   repository_id = "cloudshirt-repo"
   description   = "cloudshirt-repo voor de cloudshirt website"
   format        = "DOCKER"
 }
-
-# resource "kubernetes_deployment_v1" "default" {
-# }
